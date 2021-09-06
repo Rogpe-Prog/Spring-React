a1c5c600dc5bda3ee669e1c8ffd56a7df59fe5a2
rogpe <prog_x@hotmail.com>
2021-09-06 10:22:50 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido na Semana Spring & React <a href="https://github.com/Rogpe-Prog" target="_blank" rel="noreferrer">Rogpe</a></p>
+                <p className="text-light"><small><strong>Inspirado por Prof. Nélio Alves</strong><br/></small></p>
+            </div>
+        </footer>  
+    );
+  }
+  
+  export default Footer;
+  
+
+
+
