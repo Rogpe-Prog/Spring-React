04e9380e58c42e8109b01c5a46e39e9d5f2c6259
rogpe <prog_x@hotmail.com>
2021-09-08 20:28:34 -0300

    Pagination

---
+++ backend/src/main/java/dev/hereos/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package dev.hereos.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.hereos.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
---
+++ backend/src/main/java/dev/hereos/dsvendas/services/SaleService.java
@@ -0,0 +1,24 @@
+package dev.hereos.dsvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import dev.hereos.dsvendas.dto.SaleDTO;
+import dev.hereos.dsvendas.entities.Sale;
+import dev.hereos.dsvendas.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+	
+	public Page<SaleDTO> findAll(Pageable pageable){
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+
+	
+}
